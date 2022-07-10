25d763a2b1bd51b1219f3d8f48083a7801fed85e
jucadura <93133974+jucadura@users.noreply.github.com>
2022-07-10 18:44:35 -0500

    Update Sistema.java

--- Proyecto1erParcial/src/main/java/Package1/Sistema.java
+++ Proyecto1erParcial/src/main/java/Package1/Sistema.java
@@ -97,15 +97,52 @@ public class Sistema {
                     opcion=sc.nextInt();
                     switch (opcion){
                         case 1:
-                            System.out.println("/*****RESERVACION*****/\n/*                      */\n/*********************/");
+                           System.out.println("/*****RESERVACION*****/\n/*                   */\n/*********************/");
                             System.out.println("Ingrese fecha de entrada (dd-MM-AAAA): ");
                             sc.nextLine();
                             String entrada = sc.nextLine();
                             System.out.println("Ingrese fecha de salida(dd-MM-AAAA): ");
                             String salida = sc.nextLine();
+                            int dias = Integer.valueOf(salida.substring(0,1));
+                            Integer.valueOf(entrada.substring(0,1));
                             LocalDate fechaInicio = formatearFecha(entrada);
-                            System.out.println(fechaInicio);
-                            break;
+                            LocalDate fechaFin = formatearFecha(salida);
+                             
+                            System.out.println("¿Qué tipo de hospedaje busca?\n1. Hotel\n2. Departamento");
+                            int hospedaje = sc.nextInt();
+                            switch (hospedaje){
+                                case 1:
+                                    ArrayList<String> hotels = LeeFichero("hoteles.txt");
+                                    ArrayList<Hotel> hoteles = new ArrayList<>();
+                                    for(int i=1; i<hotels.size();i++){
+                                        String[] h = hotels.get(i).split(",");
+                                        Hotel hotel = new Hotel(h[0],h[1],Integer.valueOf(h[2]),h[3],Boolean.getBoolean(h[4]),Boolean.getBoolean(h[5]),Boolean.getBoolean(h[6]),fechaInicio,fechaFin);
+                                        hoteles.add(hotel);
+                                    }
+                                    System.out.println("Estos son los hoteles disponibles: ");
+                                    for (int i=0; i<hoteles.size();i++){
+                                        System.out.println((i+1)+".- "+hoteles.get(i).getNombre());
+                                    }
+                                    sc.nextLine();
+                                    int eleccion = sc.nextInt();
+                                    Hotel elegido = hoteles.get(eleccion-1);
+                                    elegido.mostrarInfo();
+                                    
+                                    System.out.println("Elija el tipo de habitacion que desea:");
+                                    elegido.buscarHabitaciones();
+                                    elegido.mostrarTipo();
+                                    String[] tips = {"INDIVIDUAL" , "DOBLE" , "FAMILIAR"};
+                                    System.out.println("\nElija una opcion");
+                                    int tipoElec = sc.nextInt();
+                                    sc.nextLine();
+                                    ArrayList<Habitacion> dispo = elegido.habitacionesDisponibles(TipoHabitacion.valueOf(tips[tipoElec-1]));
+                                    Habitacion habChosen;
+                                    if (dispo.isEmpty()==false){
+                                         habChosen = dispo.get((int)Math.random()*dispo.size());
+                                        System.out.println("Usted ha elegido una habitacion "+habChosen.getTipo().toString().toLowerCase()+" para el total de "+dias+"noche");
+                                        System.out.println("El costo a pagar es de "+habChosen.getPrecio()+" dolares\n¿Desea reservar?");
+                                        String resQ= sc.nextLine().toLowerCase();
+                                        if (resQ == "si"){
                             
                             
                         
