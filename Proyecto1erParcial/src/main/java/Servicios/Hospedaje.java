3c35a2735f11cec0ea5f342b34e248d4feaf0d14
jucadura <93133974+jucadura@users.noreply.github.com>
2022-07-10 18:57:28 -0500

    Update Hospedaje.java

--- Proyecto1erParcial/src/main/java/Servicios/Hospedaje.java
+++ Proyecto1erParcial/src/main/java/Servicios/Hospedaje.java
@@ -23,7 +23,26 @@ public class Hospedaje {
         this.fechaInicio = fechaInicio;
         this.fechaFin = fechaFin;
     }
+     @Override
+    public String toString() {
+        return "Hospedaje{" + "fechaInicio=" + fechaInicio + ", fechaFin=" + fechaFin + '}';
+    }
+    
+    public  void mostrarInfo(){
+        System.out.println("Fecha Inicial: " + fechaInicio + "\n Fecha Final: " + fechaFin);
+    }
 
     
+    public Reserva generarReserva(Cliente c,LocalDate fechaInicio, LocalDate fechaFinal,int valor){
+        this.setValor(valor);
+        Date date = new Date();  
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");  
+        String strDate = formatter.format(date);  
+        System.out.println(strDate);
+        Reserva r = new Reserva(this.getId(),c,TipoServicio.HOSPEDAJE,fechaInicio,fechaFinal);        
+        String resStr = r.getCodigo()+","+strDate+","+TipoServicio.HOSPEDAJE.toString().toLowerCase()+","+r.getCliente().getNombre()+","+this.valor;
+        EscribirArchivo("reservas.txt",resStr);
+        return r;
+    }
     
 }
