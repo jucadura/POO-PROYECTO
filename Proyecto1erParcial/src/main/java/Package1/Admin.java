1c397848ebe1a551d99e1d511737d04afbee51c3
jucadura <93133974+jucadura@users.noreply.github.com>
2022-07-10 23:04:34 -0500

    Update Admin.java

--- Proyecto1erParcial/src/main/java/Package1/Admin.java
+++ Proyecto1erParcial/src/main/java/Package1/Admin.java
@@ -11,9 +11,18 @@ package Package1;
  */
 public class Admin extends Usuario{
     private int totalReservas;
-
-    public Admin(String cedula, String nombre, String apellido, String user, String contraseña, String celular, TipoUsuario tipoU) {
+    
+    public Admin(int totalReservas, String cedula, String nombre, String apellido, String user, String contraseña, String celular, TipoUsuario tipoU) {
         super(cedula, nombre, apellido, user, contraseña, celular, tipoU);
+        this.totalReservas = totalReservas;
+    }
+
+    public int getTotalReservas() {
+        return totalReservas;
+    }
+
+    public void setTotalReservas(int totalReservas) {
+        this.totalReservas = totalReservas;
     }
 
     @Override
