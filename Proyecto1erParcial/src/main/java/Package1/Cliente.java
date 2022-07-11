d830a8b1ef522e418dba253c23cefe878414fe97
nei1602 <87281975+nei1602@users.noreply.github.com>
2022-07-10 23:39:05 -0500

    Update Cliente.java

--- Proyecto1erParcial/src/main/java/Package1/Cliente.java
+++ Proyecto1erParcial/src/main/java/Package1/Cliente.java
@@ -40,8 +40,17 @@ public class Cliente extends Usuario {
     public String toString() {
         return super.toString()+" Cliente{" + "edad=" + edad + ", numTarjeta=" + numTarjeta ;
     }
-    
-
-   
-    
+    public Reserva generarReserva(Hotel hotel,Habitacion hab){
+        Reserva r = new Reserva(this);
+        double cod= Math.random()*10000;
+        r.setCodigo((int)cod+"");
+        Date date = new Date();  
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");  
+        String strDate = formatter.format(date);  
+        String resStr = r.getCodigo()+","+strDate+","+TipoServicio.HOSPEDAJE.toString().toLowerCase()+","+r.getCliente().getNombre()+","+hab.getPrecio();
+        EscribirArchivo("reservas.txt",resStr);
+        String resHStr = r.getCodigo()+","+hotel.getCodigo()+","+hab.getTipo().toString().toLowerCase()+","+hab.getPrecio();
+        EscribirArchivo("reservasHospedaje.txt",resHStr);
+        return r;
+    }
 }
